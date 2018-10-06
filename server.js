41f25721f77810443b86bc6f3489ee638bdffef8
Shane Tajima <shanetajima@gmail.com>
2018-10-06 16:02:42 -0700

    Change new relic path

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('./tests/new_relic/albumlist_newrelic.js');
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
