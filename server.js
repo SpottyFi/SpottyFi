51f959a2a659c8b5e2669367ae15ccc09632fc1c
Shane Tajima <shanetajima@gmail.com>
2018-10-12 13:29:33 -0700

    Update for EC2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
@@ -7,28 +7,28 @@ var compression = require('compression');
 var server = express();
 server.use(compression())
 server.use(bodyParser.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, './'), { maxAge: '30 days' }));
 server.use(cors());
 
 // Albums & Player
 server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://52.15.129.193' + req.url);
+  res.redirect('http://52.38.108.191' + req.url);
 });
 
 // Related Artists
 server.get('/artist/:id/relatedArtists', (req, res) => {
-  res.redirect('http://18.206.245.56' + req.url);
+  res.redirect('http://52.53.165.223' + req.url);
 });
 
 // Popular Songs
 server.get('/artist/:id', (req, res) => {
-  res.redirect('http://18.224.17.253' + req.url);
+  res.redirect('http://54.176.212.76' + req.url);
 });
 
 // Header
 server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://35.172.133.115' + req.url);
+  res.redirect('http://35.182.64.129' + req.url);
 });
 
-server.listen(3000, console.log('Listening on:', 3000));
+server.listen(80, console.log('Listening on:', 80));
